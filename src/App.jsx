b56423be60c3727e7bafb19fe8b7952b10a97b5d
猪狩 慶伍 <keigo-igari@cresco-jcube.co.jp>
2025-03-31 11:56:27 +0900

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import React, { useState } from 'react';
 import reactLogo from "./assets/pokemon.png";
 import "./App.css";
 
 function App() {
+  
+  const [isGlowing, setIsGlowing] = useState(false);
+  const toggleGlow = () => {
+    setIsGlowing(!isGlowing);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img
+          src={reactLogo}
+          className={`logo react ${isGlowing ? 'glowing' : ''}`}
+          alt="React logo"
+        />
 
         <h1>ポケモン</h1>
+        <button onClick={toggleGlow}>ボタン</button>
       </header>
     </div>
   );
