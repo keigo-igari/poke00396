714bb5c64fda7dafbd3f5b29a4bb0631d0581f9a
猪狩 慶伍 <keigo-igari@cresco-jcube.co.jp>
2025-03-31 11:34:26 +0900

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-//import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // React Router v6に合わせて修正
 import reactLogo from "./assets/pokemon.png";
 import "./App.css";
-import NewComponent from "./newTest"; // 新しいコンポーネントをインポート
 
 function App() {
   return (
-    <Router> {/* Routerでアプリ全体を囲む */}
-      <div className="App">
-        <header className="App-header">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
 
-          <h1>Hello</h1>
-          
-          {/* ボタンをクリックしたときにNewComponentに遷移 */}
-          <Link to="/new-page">
-            <button>ボタン</button>
-          </Link>
-        </header>
-        
-        {/* Routesに変更 */}
-        <Routes>
-          <Route path="/new-page" element={<NewComponent />} />
-          {/* 他のルートを必要に応じて追加 */}
-        </Routes>
-      </div>
-    </Router>
+        <h1>ポケモン</h1>
+      </header>
+    </div>
   );
 }
 
